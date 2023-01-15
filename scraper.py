f8acf4e36a326c6d519ccff8d29dac7761ec0027
Francisco Nycolas Sales Dias <nycolas.dias@GRUPOOK.LOCAL>
2023-01-15 13:00:37 -0300

    v1 - versao sem automacao de paginacao

---
+++ scraper.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+from time import sleep
+from bs4 import BeautifulSoup
+import datetime
+
+# Requisitando página inicial
+options = Options()
+options.add_argument('--headless')
+options.add_argument('window-size=600,1200')
+links= []
+lista_reclamacoes = []
+url_base = 'https://www.reclameaqui.com.br'
+
+
+
+def main(x):
+    url = f'https://www.reclameaqui.com.br/empresa/sou-energy/lista-reclamacoes/?pagina={x}' 
+    links = []
+    # Pegando os dados do site e todas as reclamacoes da pagina
+    navegador = webdriver.Chrome(options=options)
+    sleep(2)
+    navegador.get(url)
+    sleep(2)
+    page_content = navegador.page_source
+    site = BeautifulSoup(page_content, 'html.parser')
+    
+   
+    urls = site.findAll('div', attrs={'class': 'sc-1pe7b5t-0 bJdtis'}) # Conjunto com LINKS da pag
+
+    for i in urls:
+        link_req = url_base +i.find('a')['href']
+        links.append(link_req)
+    print(links)    
+    
+    for link in links:
+        sleep(2)
+        navegador.get(link)
+        sleep(2)
+        # print(navegador)
+        soup = BeautifulSoup(navegador.page_source, 'html.parser')
+        titulo_reclamacao = soup.find('h1', attrs={'class': 'lzlu7c-3 berwWw'}).text
+        localizacao = soup.find('span', attrs={'data-testid': 'complaint-location'}).text
+        data_criacao = soup.find('span', attrs={'data-testid': 'complaint-creation-date'}).text
+        id_reclamacao = soup.find('span', attrs={'data-testid': 'complaint-id'}).text
+        status_reclamacao = soup.find('div', attrs={'data-testid': 'complaint-status'}).text
+        try:
+            compraria_novamente = soup.find('div', attrs={'data-testid': 'complaint-deal-again'}).text
+            nota_atendimento = soup.find('div', attrs={'class': 'uh4o7z-3 ceUcTc'}).text
+            consideracao_final_consumidor = soup.find('p', attrs={'class': 'sc-1o3atjt-4 JkSWX'}).text
+        except:
+            compraria_novamente = ''
+            nota_atendimento = ''
+            consideracao_final_consumidor = ''
+            
+        reclamacao = {
+            'titulo': titulo_reclamacao,
+            'localizacao': localizacao,
+            'data_criacao': data_criacao,
+            'id_reclamacao': id_reclamacao,
+            'status': status_reclamacao,
+            'compraria_novamente': compraria_novamente,
+            'nota_atendimento': nota_atendimento,
+            'consideracao_final_consumidor': consideracao_final_consumidor
+        }
+
+        lista_reclamacoes.append(reclamacao)
+
+        
+    return lista_reclamacoes
+    
+for x in range(1,16):
+    main(x)
+    
+df = pd.DataFrame(lista_reclamacoes)
+df.to_csv(f'.\data\data-{datetime.date.today()}.csv', index=False) 
+  
+    
+    
+# try:
+#     main()
+#     df = pd.DataFrame(lista_reclamacoes)
+#     df.to_csv(f'.\data\data-{datetime.date.today()}.csv', index=False)
+# except Exception as e:
+#     print(e)
+# print(df_p)
